41d28e2a82929b49197b9ee2686dbd021e113824
Gerardo Rodriguez <gerarodriguez@proton.me>
2024-05-17 17:08:01 -0500

    finished Hero component"

--- frontend/src/components/Hero.jsx
+++ frontend/src/components/Hero.jsx
@@ -8,12 +8,15 @@ const Hero = ({
     <main className="bg-indigo-700 py-20 mb-4">
         {/* <div className="border border-white max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 flex flex-col items-center">             */}
             {/* <div className="border border-black text-center"> */}
-            <div className="border border-black max-w-7xl mx-auto text-center">            
+            <div className="max-w-7xl mx-auto text-center">            
                 <h1 className="text-4xl font-extrabold text-white sm:text-5xl md:text-6xl">
                     {title}
                 </h1>
-                <p className="my-4 text-xl text-white">
+                <h2 className="my-4 text-3xl text-white">
                     {subtitle}
+                </h2>
+                <p className="my-4 text-2xl text-white">
+                    Now, you too, can "jaja"!
                 </p>
             </div>
         {/* </div> */}
